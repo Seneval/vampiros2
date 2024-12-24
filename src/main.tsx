286f08f7b6d8d4443009155d2eb3ae2b8efcebf7
Patricio Horacio Montes Lozano <patricio@192.168.1.11>
2024-12-24 00:17:20 -0600

    Fix and validate all TypeScript files

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'; // Importa el componente principal
-import './style.css'; // Importa estilos globales
+import App from './App';
+import './style.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
